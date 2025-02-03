3fa51fa2d475a63b8cbe6e50acdcd714250d2283
Rudra <rudra.phymos@gmail.com>
2025-02-03 22:24:50 +0530

    Navbar and Services card

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import logo from "../../public/assets/logo.svg";
+
+const Navbar = () => {
+  const navMenu = (
+    <>
+      <li>
+        <Link href={"/"}>Home</Link>
+      </li>
+      <li>
+        <Link href={"/about"}>About</Link>
+      </li>
+      <li>
+        <Link href={"/services"}>Services</Link>
+      </li>
+      <li>
+        <Link href={"/blog"}>Blog</Link>
+      </li>
+      <li>
+        <Link href={"/contact"}>Contact</Link>
+      </li>
+    </>
+  );
+
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {navMenu}
+            </ul>
+          </div>
+          <Link href="/" className="text-xl">
+            <Image src={logo} width={107} height={87}></Image>
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navMenu}</ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn btn-outline text-[#FF3811]">Appointment</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
